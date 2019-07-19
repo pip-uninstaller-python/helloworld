8e3460fdc5f08a763a7a0d8605cbbf78a4944eb2
秃如其来 <hoaprox@qq.com>
2019-07-19 10:01:59 +0800

    acm_buycocacola
    acm_buycocacola

---
+++ exercise/A pure code/acm_buycocacola.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# 超市里的可乐进行大优惠，每买2瓶送2瓶（买3瓶因为只包含一个2瓶，
+# 所以只送2瓶，4瓶可以送4瓶）小明买了n瓶可乐，请你编写一个程序，
+# 算算他一共能拿走几瓶可乐。
+
+def buy():
+    num = input("输入购买数量：")
+    n = int(num)
+    if (n % 2 == 0):
+        count = n / 2
+    else:
+        n = n - 1
+        count = n / 2
+    return count + int(num)
+
+
+num = buy()
+print(num)
