cbfb71f06f6c7503105c18553812867013fd106a
commitsession <hoaprox@qq.com>
2019-07-22 17:07:21 +0800

    remove dir

--- exercise/chenshouquan/参数传入关键词传入.py
+++
@@ -1,5 +0,0 @@
-#参数的传入：关键词传入
-def show(name,age,sex,hobby):#形参方法定义参数
-	print("我叫：",name,"年龄：",age,"性别：",sex,"爱好:",hobby)#字符串和参数之间要用逗号隔开
-
-show(name="张三",sex="男",hobby="打球",age=18)#关键词传入 全部用关键词传入
--- exercise/chenshouquan/执行打印1000次hello world.py
+++
@@ -1,6 +0,0 @@
-#打印执行1000次hello world！
-for i in range(1000):#i是从0到999合计1000次 
-     #print("hello world！")#挎号内写上hello world！，写i就显示次数。
-     #print("第",i,"hello world！")#加上i显示次数/第 999 hello world！
-     #print("第",i+1,"次打印 hello world！")#加上i=1显示次数/第 1000 次打印 hello world！
-     
