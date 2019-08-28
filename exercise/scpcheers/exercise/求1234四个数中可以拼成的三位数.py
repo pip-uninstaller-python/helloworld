f93bca2f4ccbb52ccf84209c8dbc3fd6151c02c1
scpcheers <568697692@qq.com>
2019-08-28 18:25:50 +0800

    exercise -- scpcheers

--- exercise/scpcheers/exercise/求1234四个数中可以拼成的三位数.py
+++ exercise/scpcheers/exercise/求1234四个数中可以拼成的三位数.py
@@ -1,3 +1,4 @@
+# 总共的
 b = []
 for  x in range(1,5):
     for y in range(1,5):
@@ -5,3 +6,11 @@ for  x in range(1,5):
             b.append(int(f'{x}{y}{z}'))
 print(f'总共有{len(b)}个数.\n分别是：')
 print([i for i in b])
+
+# 互不相同且没有相同数字
+b = []
+for  x in range(1,5):
+    for y in range(1,5):
+        for z in range(1,5):
+            if (x!=y) and (x!=z) and (y!=z):
+                print(x,y,z)
